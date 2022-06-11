8ea9e410bf45a7867ed20fc206ea2f0d6c4a2c48
Kacper Lewicki <kacper.lewicki@onet.pl>
2022-06-11 10:24:40 +0200

    Updates

---
+++ precache-manifest.ae0ed40215fd32cca9e5fe02cb63761a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3fa96f2ef36c0695e06",
+    "url": "/React-app/static/css/main.3452b508.chunk.css"
+  },
+  {
+    "revision": "b3fa96f2ef36c0695e06",
+    "url": "/React-app/static/js/main.cf9b9c54.chunk.js"
+  },
+  {
+    "revision": "d704105685e9a50e8fe8",
+    "url": "/React-app/static/js/runtime~main.a7bc867e.js"
+  },
+  {
+    "revision": "620ca130e51ca15065bb",
+    "url": "/React-app/static/js/2.61fd6981.chunk.js"
+  },
+  {
+    "revision": "35cae7480aba465403c8180c351fcbb1",
+    "url": "/React-app/index.html"
+  }
+];
